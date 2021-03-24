40e9c9745b7213f52899b2105d1cd41f599439f7
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-24 14:41:35 -0300

    feat: added SignInButton component

--- ignews/src/components/Header/SignInButton/index.tsx
+++ ignews/src/components/Header/SignInButton/index.tsx
@@ -1,10 +1,18 @@
+import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
 export function SignInButton() {
-    return (
-        <button type="button">
-            <FaGithub/>
-            Sign In with github;
-        </button>
-    )
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn
+        ? (<button type="button" className={styles.SignInButton}>
+            <FaGithub color="#04d361" />
+            Gustavo Maltez
+            <FiX color="#737380" className={styles.closeIcon}/>
+           </button>)
+        : (<button type="button" className={styles.SignInButton}>
+            <FaGithub color="#eba417" />
+            Sign In with github
+          </button>)
 }
