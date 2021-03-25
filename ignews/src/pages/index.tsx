ff56caaca36902a4105e07cde9f18de6d4f3be5b
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-25 09:45:59 -0300

    feat: added stripe and getServerSideProps on home page

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,8 +1,16 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps{
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,7 +23,7 @@ export default function Home() {
           <h1>News about <br/> the <span>React</span> world</h1>
           <p>
             Get acess to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for $${product.amount} month</span>
           </p>
           <SubscribeButton/>
         </section>
@@ -24,4 +32,22 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1IYsF4JYJAaP2mjvRcJt6MOc', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
