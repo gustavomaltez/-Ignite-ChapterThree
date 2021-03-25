eabff2bff26846672a5d9136dfd580068add4df5
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-25 11:57:27 -0300

    feat: added github auth

--- ignews/src/components/Header/SignInButton/index.tsx
+++ ignews/src/components/Header/SignInButton/index.tsx
@@ -1,18 +1,25 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn
-        ? (<button type="button" className={styles.SignInButton}>
+    return session
+        ? (<button type="button"
+            className={styles.SignInButton}
+            onClick={() => { signOut() }}
+        >
             <FaGithub color="#04d361" />
-            Gustavo Maltez
-            <FiX color="#737380" className={styles.closeIcon}/>
-           </button>)
-        : (<button type="button" className={styles.SignInButton}>
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>)
+        : (<button type="button"
+            className={styles.SignInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417" />
             Sign In with github
-          </button>)
+        </button>)
 }
