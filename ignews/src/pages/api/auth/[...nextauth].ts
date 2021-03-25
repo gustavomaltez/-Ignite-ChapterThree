d64b7ecd56ea5b1e92e56f8985ba812a2c5d5838
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-25 18:36:24 -0300

    feat: added user email request in signIn callback

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,37 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+
+interface GitHubUserEmail {
+  email: string,
+  primary: boolean,
+  verified: boolean,
+  visibility: string | null
+}
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user'
+      scope: 'read:user,user:email'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+
+      const responseGetUserEmails = await fetch('https://api.github.com/user/emails', {
+        headers: {
+          'Authorization': `Bearer ${account.accessToken}`,
+        }
+      })
+
+      const userEmails = await responseGetUserEmails.json();
+
+      const { email } = userEmails.find((email: GitHubUserEmail) => email.primary)
+
+      return true;
+    }
+  }
 })
