c7c667965159c18818d4f7f2f83d7e070ceb911e
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-26 21:17:34 -0300

    feat: added manageSubscription function

--- ignews/src/pages/api/webhooks.ts
+++ ignews/src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
 
@@ -37,14 +38,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         try {
             event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
-        } catch (err){
+        } catch (err) {
             return res.status(400).send(`Webhook error: ${err.message}`);
         }
 
         const { type } = event;
 
-        if(relevantEvents.has(type)){
-            console.log('Evento Recebido', event);
+        if (relevantEvents.has(type)) {
+            try {
+                switch (type) {
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        );
+                        break;
+                    default:
+                        throw new Error('Unhandled event.');
+                }
+            } catch (err) {
+                return res.json({ error: 'Webhook handler failed' });
+            }
         }
 
         res.json({ recived: true });
