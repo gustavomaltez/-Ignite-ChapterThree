71db8ddf3ee0bef2c126d5cbceffc124921fcf90
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-30 18:31:19 -0300

    feat: added getStaticProps on posts page

--- ignews/src/services/prismic.ts
+++ ignews/src/services/prismic.ts
@@ -1,11 +1,12 @@
 import Prismic from '@prismicio/client';
 
-export function getPrismicClient() {
+export function getPrismicClient(req?: unknown) {
     const prismic = Prismic.client(
-        process.env.PRISMIC_ACCESS_TOKEN,
+        process.env.PRISMIC_ENDPOINT,
         {
-            accessToken: process.env.PRISMIC_ACCESS_TOKEN, 
+            req,
+            accessToken: process.env.PRISMIC_ACCESS_TOKEN,
         });
 
-        return prismic;
+    return prismic;
 }
