6a4c6e23e7c21838d5e87264b43db91447e3eb78
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-30 18:03:05 -0300

    feat: added posts page

---
+++ ignews/src/pages/posts/index.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts(){
+    return (
+        <>
+            <Head>
+                <title>Posts  | IGnews</title>
+            </Head>
+
+            <main>
+                <div>
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Authentication in Next.js: Building an auth API with NextAuth.js</strong>
+                        <p>Authentication is the act of proving that someone is who they say they are — confirming the identity of a user in an application, for example. In this tutorial...</p>
+                    </a>
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Authentication in Next.js: Building an auth API with NextAuth.js</strong>
+                        <p>Authentication is the act of proving that someone is who they say they are — confirming the identity of a user in an application, for example. In this tutorial...</p>
+                    </a>
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Authentication in Next.js: Building an auth API with NextAuth.js</strong>
+                        <p>Authentication is the act of proving that someone is who they say they are — confirming the identity of a user in an application, for example. In this tutorial...</p>
+                    </a>
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Authentication in Next.js: Building an auth API with NextAuth.js</strong>
+                        <p>Authentication is the act of proving that someone is who they say they are — confirming the identity of a user in an application, for example. In this tutorial...</p>
+                    </a>
+                    <a>
+                        <time>12 de março de 2021</time>
+                        <strong>Authentication in Next.js: Building an auth API with NextAuth.js</strong>
+                        <p>Authentication is the act of proving that someone is who they say they are — confirming the identity of a user in an application, for example. In this tutorial...</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    );
+}
