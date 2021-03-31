ea3d730eafb91e6572dd25a47524c7a61ff15af1
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-30 22:27:18 -0300

    feat: added regirect user if already have a subscription on click in subscribe button

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -3,6 +3,7 @@ import api from '../../services/api';
 import { getStripeJs } from '../../services/stipe-js';
 import styles from './styles.module.scss';
 import { toast } from 'react-toastify';
+import { useRouter } from 'next/dist/client/router';
 
 interface SubscribeButtonProps{
     priceId: string;
@@ -11,12 +12,18 @@ interface SubscribeButtonProps{
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
 
     const [ session ] = useSession();
+    const router = useRouter();
 
     async function handleSubscribe(){
         if(!session){
             signIn('github');
             return;
         }      
+
+        if(session?.activeSubscription){
+            router.push('/posts');
+            return;
+        }
 
         try {
             const response = await api.post('/subscribe');
